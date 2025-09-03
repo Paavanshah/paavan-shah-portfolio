8be419a7479d27916c4ed1aee7f21cdcf9e54ea6
Paavan Shah <paavanshah44@gmail.com>
2025-09-03 09:56:25 +0530

    Fix vite base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // The 'base' option is removed for local development
+  base: '/paavan-shah-portfolio/', // This line is crucial for GitHub Pages
   plugins: [react()],
 })
